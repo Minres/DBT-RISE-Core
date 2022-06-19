00c4420f04b11be07e2b43c0f3ed59a0c855d214
Eyck Jentzsch <eyck@minres.com>
2022-06-19 13:11:41 +0200

    adds instruction decoder interface definition

---
+++ src/iss/arch/instruction_decoder.h
@@ -0,0 +1,26 @@
+/*
+ * instruction_decoder.h
+ *
+ *  Created on: Jun 19, 2022
+ *      Author: eyck
+ */
+
+#ifndef _ISS_ARCH_INSTRUCTION_DECODER_H_
+#define _ISS_ARCH_INSTRUCTION_DECODER_H_
+
+#include <memory>
+
+namespace iss {
+namespace arch {
+
+template <typename ARCH> struct instruction_decoder {
+
+    static std::unique_ptr<instruction_decoder<ARCH>> create();
+
+    template<typename T>
+    unsigned decode_instruction(T);
+};
+}
+}
+
+#endif /* _ISS_ARCH_INSTRUCTION_DECODER_H_ */
