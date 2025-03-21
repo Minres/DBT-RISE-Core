6b3558e671e24c052eb9c97bb9ff6597b7621e47
Eyck Jentzsch <eyck@minres.com>
2025-03-21 20:49:35 +0100

    changes end of run handling for DBT backends

--- src/iss/llvm/vm_base.h
+++ src/iss/llvm/vm_base.h
@@ -168,7 +168,7 @@ public:
                         // execute the compiled function
                         pc.val = reinterpret_cast<func_ptr>(cur_tb->f_ptr)(regs_base_ptr, arch_if_ptr, vm_if_ptr);
                         if(core.should_stop() || last_branch == BRANCH_TO_SELF)
-                            break;
+                            throw simulation_stopped(0);
                         // update last state
                         last_tb = cur_tb;
                         // if the current tb has a successor assign to current tb
